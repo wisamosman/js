1d597a6a0c24e73c19169f9e3ce64edea57be720
wisamosman <viesam_1987@hotmail.com>
2023-02-16 18:51:37 +0100

    add html

---
+++ script.js
@@ -0,0 +1,9 @@
+alert("hello wesam")
+document .getElementById("SA").innerHTML="ANA ABN MERYEM";
+document.getElementById("SA").innerHTML="wrej";
+console.log("hello hgfdssajkgufkgoufolo")
+let userName='wesam'
+,age ='25'
+,message='hello'
+console.log('userName');
+const my='10.01.1986'
