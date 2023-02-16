39ca34101dcf8f9f29feb3f1f1da4d90b4c69695
wisamosman <viesam_1987@hotmail.com>
2023-02-16 19:09:24 +0100

    add html3

--- script.js
+++ script.js
@@ -4,9 +4,12 @@ document.getElementById("SA").innerHTML="wrej";
 console.log("hello hgfdssajkgufkgoufolo")
 let userName='wesam'
 ,age ='25'
+,married
 ,message='hello'
 console.log('userName');
 const my='10.01.1986'
 alert('welcom')
-let Age = prompt('Enter Your Age : ')
+let Age = prompt('Enter Your Age : 31')
 console.log(age)
+let Ana = confirm('Are you married ?')
+console.log('Ana')
